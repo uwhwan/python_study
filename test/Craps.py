48f587e09226bee844ee3e65d8e46af8849a9723
uwh <uwhwan@163.com>
2020-10-12 17:38:06 +0800

    添加注释

--- test/Craps.py
+++ test/Craps.py
@@ -4,11 +4,13 @@ money = 1000
 while money > 0:
     print('资产为：'+str(money))
     go_on = False
+    #下注金额必须大于0，小于等于玩家总资产
     while True:
         debt = int(input('请下注：'))
         if 0 < debt <= money:
             break
-
+    #第一次摇色子
+    #用1到6均匀分布的随机数模拟摇色子得到的点数
     first = randint(1,6)+randint(1,6)
     print('玩家摇出了'+str(first)+'点')
     if first == 7 or first ==11:
@@ -19,6 +21,7 @@ while money > 0:
         money -= debt
     else:
         go_on = True
+    #第一次摇色子没有分出胜负游戏继续
     while go_on:
         go_on = False
         current = randint(1,6)+randint(1,6)
